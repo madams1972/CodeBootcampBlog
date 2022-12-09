cac59ae9cb51b08880c57d4eaf4a7ca8f7c7bf17
fridayknight13 <73671454+madams1972@users.noreply.github.com>
2022-12-09 15:39:01 -0600

    updateduserRoutes.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
+const commentRoutes = require('./commentRoutes');
+
 
 router.use('/users', userRoutes);
 router.use('/post', postRoutes);
