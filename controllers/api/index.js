2bfc197d6a43defe659443f4fb0b7ca05e112517
fridayknight13 <73671454+madams1972@users.noreply.github.com>
2022-12-09 15:35:30 -0600

    changed file names and updated routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,9 @@
 const router = require('express').Router();
-
-const userRoutes = require('./user-routes.js');
-const postRoutes = require('./post-routes');
-const commentRoutes = require('./comment-routes');
+const userRoutes = require('./userRoutes');
+const postRoutes = require('./postRoutes');
 
 router.use('/users', userRoutes);
-router.use('/posts', postRoutes);
-router.use('/comments', commentRoutes);
+router.use('/post', postRoutes);
+router.use('/comment', commentRoutes);
 
 module.exports = router;
